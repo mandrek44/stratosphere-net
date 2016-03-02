1509916401ad44dc6037b1c3ec5313adec6e8d29
Marcin Drobik <mdrobik@future-processing.com>
2016-03-02 07:30:53 +0100

    Column Major matrix and column and row major indexing

--- Sources/Stratosphere.Math/Matrix.cs
+++ Sources/Stratosphere.Math/Matrix.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace Stratosphere.Math
 {
     public class Matrix
     {
+    }
+
+    public class ColumnMajorMatrix
+    {
+        private readonly double[] _data;
+        private readonly int[] _dimensions;
+
+        public ColumnMajorMatrix(double[] data, int[] dimensions)
+        {
+            _data = data;
+            _dimensions = dimensions;
+        }
+
+        public ColumnMajorMatrix Multiply(double scalar)
+        {
+            return new ColumnMajorMatrix(
+                _data.Select(value => value*scalar).ToArray(),
+                _dimensions.ToArray());
+        }
+
+        public double GetByColumnIndex(int index) => _data[index];
+
+        public double GetByRowIndex(int index)
+        {
+            var row = index/_dimensions[1];
+            var column = index % _dimensions[1];
+
+            return _data[row + column*_dimensions[0]];
+        }
     }
 }
