17d4f74f2c2248fdcb104f125b0a37d5bc51890e
Marcin Drobik <mdrobik@future-processing.com>
2016-03-09 07:26:49 +0100

    Minor refactor

---
+++ Sources/Stratosphere.Math/Matrix/ColumnsConcatMatrix.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stratosphere.Math.Matrix
+{
+    public class ColumnsConcatMatrix : Matrix
+    {
+        private readonly Matrix _a;
+        private readonly Matrix _b;
+
+        public ColumnsConcatMatrix(Matrix a, Matrix b)
+        {
+            if (a.Height != b.Height)
+                throw new InvalidOperationException("Matrices must be of equal height");
+
+            _a = a;
+            _b = b;
+
+            Size = new int[] {_a.Height, _a.Width + _b.Width};
+        }
+
+        public override int[] Size { get; }
+
+        public override IEnumerable<int> IndexesByRows() => IndexesByColumns();
+        
+        public override IEnumerable<int> IndexesByColumns() => Enumerable.Range(0, Size.Product());
+
+        public override double GetByColumnIndex(int columnIndex)
+        {
+            if (columnIndex < _a.Size.Product())
+                return _a.GetByColumnIndex(columnIndex);
+            else
+                return _b.GetByColumnIndex(columnIndex - _a.Size.Product());
+        }
+
+        public override double GetByRowIndex(int rowIndex) => GetByColumnIndex(rowIndex);
+
+        public override double GetByCoordinates(int row, int column)
+        {
+            if (column < _a.Width)
+                return _a.GetByCoordinates(row, column);
+            else
+                return _b.GetByCoordinates(row, column - _a.Width);
+        }
+
+        protected override bool Equals(Matrix other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
